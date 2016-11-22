6d6886349b51516a5db2cce956475aa3d2346d05
Mike Lorey <mike.lorey@magicjack.com>
2016-11-22 15:55:19 -0500

    Starting to work on site navigation

--- _dev/src/js/scripts.js
+++ _dev/src/js/scripts.js
@@ -1,7 +1,30 @@
-// if ( window.location.pathname == '/' ){
-//     // Index (home) page
-//     console.log(window.location.pathname);
-// } else {
-//     // Other page
-//     console.log(window.location.pathname);
-// }
+// Set Headroom classes and offset
+$(document).ready(function() {
+	$(".site-header").headroom({
+	  "tolerance": 5,
+	  "offset": 205,
+	  "classes": {
+	    "initial": "animated",
+	    "pinned": "slideDown",
+	    "unpinned": "slideUp",
+	    "top": "headroom--top",
+	    "notTop": "headroom--not-top"
+	  }
+	});
+});
+
+// Smooth page scroll to an anchor on the same page
+$(function() {
+  $('a[href*="#"]:not([href="#"]):not([data-toggle="collapse"]):not(.quote-link)').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
